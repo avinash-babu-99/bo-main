015e71c574dba41e97246b686f72d026ead2467a
avinash-babu-99 <avinashbabu2312@gmail.com>
2022-09-23 08:54:38 +0530

    built in validation changes

--- bo-main/models/getModel.js
+++ bo-main/models/getModel.js
@@ -1,48 +1,60 @@
 const mongoose = require("mongoose");
-const slugify = require("slugify")
+const slugify = require("slugify");
 
-const DBSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const DBSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "name is necessary"],
+      unique: true,
+      trim: true,
+      maxlength: [40, "max of only 40 characters is required"],
+      minlength: [3, "min of 3 character is required"],
+      // we can add enumeration if needed
+      // enum: {
+      //   values: ['enum 1', 'enum 2', 'enum 3'],
+      //   message: 'error can be sent here'
+      // }
+    },
+    age: {
+      type: Number,
+      default: 1,
+      min: [1, "min age should be 1"],
+      max: [100, "max age should be 100"],
+    },
+    value: {
+      type: Number,
+    },
+    alias: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    slug: String,
+    secretAccount: {
+      type: Boolean,
+      default: false,
+    },
   },
-  age: {
-    type: Number,
-  },
-  value: {
-    type: Number,
-  },
-  alias: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false
-  },
-  slug: String,
-  secretAccount: {
-    type: Boolean,
-    default: false,
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   }
-},
-{
-  toJSON: {virtuals: true},
-  toObject: {virtuals: true}
-}
 );
 
 // it will be only virtual in the output, won't be saved in the DB
-DBSchema.virtual('virtualPropertValueConversion').get(function(){
-  return this.value * 2
-})
-
+DBSchema.virtual("virtualPropertValueConversion").get(function () {
+  return this.value * 2;
+});
 
 // Document middleware - runs before .save() and .create()
-DBSchema.pre('save',function(next){
+DBSchema.pre("save", function (next) {
   // this in the console points to the processing document
-// console.log(this)
-this.slug = slugify(this.name, {lower: true})
-next()
-})
+  // console.log(this)
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
 
 // DBSchema.post('save', function(doc, next){
 //   console.log(doc)
@@ -54,23 +66,25 @@ next()
 //   next()
 // })
 
-DBSchema.pre(/^find/, function(next){
-  this.find({secretAccount: {$ne: true}})
-  next()
-})
-DBSchema.post(/^find/, function(doc, next){
+DBSchema.pre(/^find/, function (next) {
+  this.find({ secretAccount: { $ne: true } });
+  next();
+});
+DBSchema.post(/^find/, function (doc, next) {
   // console.log(doc)
-  next()
-})
-DBSchema.pre('aggregate',function(next){
-  this.pipeline().unshift({$match: {
-    secretAccount: {
-      $ne: true
-    }
-  }})
+  next();
+});
+DBSchema.pre("aggregate", function (next) {
+  this.pipeline().unshift({
+    $match: {
+      secretAccount: {
+        $ne: true,
+      },
+    },
+  });
   // console.log(this.pipeline())
-  next()
-})
+  next();
+});
 
 const DBModel = mongoose.model("prac2", DBSchema);
 
